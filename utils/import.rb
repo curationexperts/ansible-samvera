2179cbd14bc20041bed537645a98241489239e77
McClain Looney <m@loonsoft.com>
2021-07-06 12:23:51 -0500

    Adds import & export scripts for migration

---
+++ utils/import.rb
@@ -0,0 +1,25 @@
+# Usage: ruby import.rb
+# This script imports the export package created by export.rb
+require 'fileutils'
+require 'json'
+
+abort "Error: curl not found" unless system("which curl > /dev/null")
+config = "/opt/tenejo/current/.env.production"
+abort "config file not found: #{config}" unless File.exists?(config)
+
+puts "Extracting"
+system("tar -xzf export.tgz")
+FileUtils.chown_R("tomcat", "tomcat", "fedora")
+fedora_pid = Process.spawn(%Q{curl -X POST -d "#{Dir.pwd}/fedora" "localhost:8080/fedora/rest/fcr:restore"})
+dbstuff = Hash[*File.read(config).split.find_all{|x| x=~/^DATABASE_.*/}.map{|x| x.split("=")}.flatten]
+system("curl 'http://localhost:8983/solr/tenejo/replication?command=restore&name=tenejo&location=/import/solr'")
+system("/etc/init.d/redis stop")
+FileUtils.cp("redis/dump.rdb", "/var/lib/redis/dump.db")
+FileUtils.chown("redis", "redis", "/var/lib/redis/dump.db")
+system("/etc/init.d/redis restart")
+system("su postgres -c 'dropdb -U postgres --if-exists #{dbstuff['DATABASE_NAME']} && pg_restore -U postgres -c -d postgres psql/pg.dump'", exception:true) 
+system("tar -xzf derivatives/derivatives.tgz -C /opt")
+system("tar -xzf derivatives/uploads.tgz -C /opt/uploads")
+
+puts "Waiting for processes to finish"
+Process.waitall
